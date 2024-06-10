f96745e76eb362d6373485d9be0ba351d490cf32
ToshpulatovAlimardon <alimardontoshpulatov001@gmail.com>
2024-06-10 08:25:17 +0500

    added auto career text

--- script.js
+++ script.js
@@ -4,12 +4,25 @@ const careers = ["YouTuber", "Web Developer", "Freelancer", "Instructor"];
 
 let careerIndex = 0;
 
-
+let characterIndex = 0;
 
 const updateText = () => {
+  characterIndex++;
+
   containerEl.innerHTML = `
-    <h1>I am a ${careers[careerIndex]}</h1>
+    <h1>I am a ${careers[careerIndex].slice(0, characterIndex)}</h1>
   `;
+
+  if (characterIndex === careers[careerIndex].length) {
+    careerIndex++;
+    characterIndex = 0;
+  }
+
+  if (careerIndex === careers.length) {
+    careerIndex = 0;
+  }
+
+  setTimeout(updateText, 400);
 };
 
 updateText();
